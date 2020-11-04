150b3d5f589d00369775618aef4714d198bd9c6b
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-04 22:11:57 +0600

    Registration Boilerplate

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import BannerTitle from '../../images/banner-title.png';
+
 class Register extends Component {
     constructor() {
         super();
@@ -34,54 +36,93 @@ class Register extends Component {
         const { errors } = this.state;
 
         return (
-            <div className="container">
-                <section class="row">
-                    <div class="col s12">
-                        <h4 class="txt-center">Register</h4>
-                        <div class="card margin-auto">
-                            <div class="card-content">
-                                <form method="post" class="row" action="success.html">
-                                    <div class="input-field col  s12 margin-bottom-0">
-                                        <input id="person_name" type="text" class="validate"/>
-                                        <label for="person_name">Full Name</label>
-                                    </div>
-                                    <div class="input-field col l6 s12 margin-bottom-0">
-                                        <input id="email_add" type="email" class="validate"/>
-                                        <label for="email_add">Email</label>
-                                    </div>
-                                    <div class="input-field col l6 s12 margin-bottom-0">
-                                        <input id="conf_email" type="text" class="validate"/>
-                                        <label for="conf_email">Confirm Email</label>
-                                    </div>
-                                    <div class="col l6 s12 margin-bottom-0 margin-top-10">
-                                        <p>Are you a SEGi Student?</p>
-                                        <p class="padding-top-5">
-                                        <label class="margin-right-20">
-                                            <input name="group1" type="radio"  value ="yes"/>
-                                            <span>Yes</span>
-                                        </label>
-                                        <label>
-                                            <input name="group1" type="radio" value="no" />
-                                            <span>No</span>
-                                        </label>
-                                        </p>
-                                    </div>
-
-                                    {/* add condition */}
-                                    <div class="input-field col l6 s12 margin-bottom-0">
-                                        <input id="st_id" type="text" class="validate"/>
-                                        <label for="st_id">Student ID</label>
-                                    </div>
-                                    <div class="input-field col s12 margin-bottom-0 txt-center">
-                                        <button class="btn waves-effect waves-light btn-yellow " type="submit" name="action">Submit<i class="material-icons right">send</i>
-                                        </button> {/* triggers model */}
-                                    </div>
-                                </form> {/* contact form end */}
-                            </div> {/* card-content div close */}
-                        </div> {/* card div close */}
+            <body className="subpage">
+            <div className="main">
+                <section class="banner-img valign-wrapper ">
+                    <div class="banner-title">
+                        <a href="home.html">
+                            <img src={BannerTitle} alt="Ajooba Trivia" class=""></img>
+                        </a> {/* redirect to home page */}
                     </div>
-                </section>
+                </section> {/* main banner end */}
+                <div className="container">
+                    <section className="row">
+                        <div className="col s12">
+                            <h4 className="txt-center">Register</h4>
+                            <div className="card margin-auto">
+                                <div className="card-content">
+                                    <form noValidate onSubmit={this.onSubmit} className="row">
+                                        <div className="input-field col  s12 margin-bottom-0">
+                                            <input
+                                                onChange={this.onChange}
+                                                value={this.state.name}
+                                                error={errors.name}
+                                                id="name"
+                                                type="text"
+                                            />
+                                            <label for="name">Full Name</label>
+                                        </div>
+                                        <div className="input-field col  s12 margin-bottom-0">
+                                            <input
+                                                onChange={this.onChange}
+                                                value={this.state.email}
+                                                error={errors.email}
+                                                id="email"
+                                                type="email"
+                                            />
+                                            <label for="email">Email</label>
+                                        </div>
+                                        <div className="input-field col  s12 margin-bottom-0">
+                                            <input
+                                                onChange={this.onChange}
+                                                value={this.state.password}
+                                                error={errors.password}
+                                                id="password"
+                                                type="password"
+                                            />
+                                            <label for="password">Password</label>
+                                        </div>
+                                        <div className="input-field col  s12 margin-bottom-0">
+                                            <input
+                                                onChange={this.onChange}
+                                                value={this.state.password2}
+                                                error={errors.password2}
+                                                id="password2"
+                                                type="password"
+                                            />
+                                            <label for="password2">Confirm Password</label>
+                                        </div>
+                                        <div className="col l6 s12 margin-bottom-0 margin-top-10">
+                                            <p>Are you a SEGi Student?</p>
+                                            <p className="padding-top-5">
+                                            <label className="margin-right-20">
+                                                <input name="group1" type="radio"  value ="yes"/>
+                                                <span>Yes</span>
+                                            </label>
+                                            <label>
+                                                <input name="group1" type="radio" value="no" />
+                                                <span>No</span>
+                                            </label>
+                                            </p>
+                                        </div>
+
+                                        {/* add condition */}
+                                        <div className="input-field col l6 s12 margin-bottom-0">
+                                            <input id="st_id" type="text" className="validate"/>
+                                            <label for="st_id">Student ID</label>
+                                        </div>
+                                        <div className="input-field col s12 margin-bottom-0 txt-center">
+                                            <button className="btn waves-effect waves-light btn-yellow " type="submit" name="action">Submit<i class="material-icons right">send</i>
+                                            </button> {/* triggers model */}
+                                        </div>
+                                    </form> {/* contact form end */}
+                                </div> {/* card-content div close */}
+                            </div> {/* card div close */}
+                        </div>
+                    </section>
+                </div>
             </div>
+            </body>
         )
     }
 }
