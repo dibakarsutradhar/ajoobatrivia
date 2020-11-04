862f7619f72bca880a4da7f1ada90a833a9fa3ec
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-04 19:35:36 +0600

    Landing page fixed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 
 class Navbar extends Component {
     render() {
         return (
-            <div class="nav-wrapper container-fluid">
-		      	<ul class="social-media">
-		      		<li>Follow Us: </li>
-                    <li><Link to="https://www.instagram.com/ajoobatrivia/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></Link></li>
-                </ul>
-		    </div>
+            <header>
+                <nav>
+                    <div class="nav-wrapper container-fluid">
+                    <ul class="social-media">
+                        <li>Follow Us: </li>
+                        <li><a href="https://www.instagram.com/ajoobatrivia"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    </ul>
+                    </div>
+                </nav>
+            </header>
         )
     }
 }
