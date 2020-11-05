a793a572f34c38b24d268c977a7bcd22c899f232
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-05 10:17:46 +0600

    prod build

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+/*
 // Serve static assests if in Production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
@@ -46,6 +47,7 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
+*/
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
