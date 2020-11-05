1fe6ec8b2a6515e67314778dc20ad51150ee9e27
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-05 10:53:00 +0600

    prod build bug fix

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  });
 }
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
