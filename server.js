ab2eed32a595fb214024c202e4548fc12a267a30
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-05 11:56:45 +0600

    prod build client

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/users", users);
 // Serve static assests if in Production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('./client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
