94e405fc171fb622112faca727ab21b0260001ae
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-06 23:35:37 +0600

    dashboard component

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from "../../actions/authActions";
+
+class Dashboard extends Component {
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+
+    render() {
+        const { user } = this.props.auth;
+
+        return (
+            <div>
+                <h1>MAIN GAME</h1>
+            </div>
+        );
+    }
+}
+
+Dashboard.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
