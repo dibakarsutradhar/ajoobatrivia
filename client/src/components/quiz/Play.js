7058d77218f5fc7455734d509da1950dc1cc8d77
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-08 03:45:34 +0600

    functioning questions

--- client/src/components/quiz/Play.js
+++ client/src/components/quiz/Play.js
@@ -1,17 +1,56 @@
 import React, { Component, Fragment } from 'react';
+import isEmpty from 'is-empty';
+
+import questions from './qustions.json';
 
 class Play extends Component {
     constructor() {
         super();
+        this.state = {
+            questions,
+            currentQuestion: {},
+            nextQuestion: {},
+            answer: '',
+            numberOfQuestions: 0,
+            numberOfAnsQuestions: 0,
+            currentQuestionIndex: 0,
+            score: 0,
+            correctAnswers: 0,
+            wrongAnswers: 0,
+            fiftyFifty: 2,
+            usedFiftyFifty: false,
+            time: {}
+        };
     }
 
-    increaseCount = () => {
-        this.setState({
-            counter: 5
-        });
-    }
+    componentDidMount() {
+        const { questions, currentQuestion, nextQuestion } = this.state;
+        this.displayQuestions(this.state.questions, this.state.currentQuestion, this.state.nextQuestion);
+    };
+
+    displayQuestions = (
+        questions = this.state.questions,
+        currentQuestion,
+        nextQuestion
+    ) => {
+        const { currentQuestionIndex } = this.state;
+        if(!isEmpty(this.state.questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex + 1];
+            const answer = currentQuestion.answer;
+            
+            this.setState({
+                currentQuestion,
+                nextQuestion,
+                answer
+            });
+        }
+    };
 
     render() {
+        const { currentQuestion } = this.state;
+
         return (
             <Fragment>
                 <div className="questions">
@@ -26,14 +65,14 @@ class Play extends Component {
                             20<span className="mdi mdi-clock-outline mdi-24px"></span>
                         </p>
                     </div>
-                    <h5>Question 1</h5>
+                    <h5>{currentQuestion.question}</h5>
                     <div className="options-container">
-                        <p className="option">Answer A</p>
-                        <p className="option">Answer B</p>
+                        <p className="option">{currentQuestion.optionA}</p>
+                        <p className="option">{currentQuestion.optionB}</p>
                     </div>
                     <div className="options-container">
-                        <p className="option">Answer C</p>
-                        <p className="option">Answer D</p>
+                        <p className="option">{currentQuestion.optionC}</p>
+                        <p className="option">{currentQuestion.optionD}</p>
                     </div>
 
                     <div className="button-container">
