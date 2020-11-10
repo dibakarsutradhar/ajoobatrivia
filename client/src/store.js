1ac65d8ccae9bac9febebff91468b14877a718f2
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-11-10 22:00:42 +0600

    waiting lobby release 1.0

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
@@ -10,19 +10,21 @@ const middleware = [thunk];
 const store = createStore( 
     rootReducer,
     initialState,
+    /*
     compose(            // Temp Compose Middleware for Localhost
         applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
     )
+    */
     // Production Middleware
-    /*composeWithDevTools(
+    composeWithDevTools(
         applyMiddleware(...middleware),
         window.navigator.userAgent.includes("Chrome")
         ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
         : compose
-    )*/
+    )
 );
 
 export default store;
